445a5a58501b7bc85a1faeebcab1655072f4d25a
Andrew Jackson <andrew@codechimp.org>
2025-11-05 11:58:10 +0000

    Fix imports

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -4,9 +4,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 from unittest.mock import AsyncMock, Mock, patch
-import logging
-
-import pytest
 
 import aiotsmart
 from aiotsmart.discovery import TSmartDiscovery
